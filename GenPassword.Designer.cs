c5a13d6273fa2557ebaefe3e4dcff7a53d7310a9
OLECSII MIR <zachhytnyk@live.com>
2021-12-26 19:36:36 +0200

    Доделать

--- GenPassword.Designer.cs
+++ GenPassword.Designer.cs
@@ -40,6 +40,8 @@
             this.NumbersCheck = new System.Windows.Forms.CheckBox();
             this.SpecialSymbolsCheck = new System.Windows.Forms.CheckBox();
             this.AllCheck = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -87,11 +89,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(328, 140);
+            this.button1.Location = new System.Drawing.Point(285, 140);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.Size = new System.Drawing.Size(153, 23);
             this.button1.TabIndex = 4;
-            this.button1.Text = "Сгенерировать";
+            this.button1.Text = "Сгенерировать пароль";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -165,11 +167,31 @@
             this.AllCheck.Text = "Все";
             this.AllCheck.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(341, 257);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Скопировать";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(241, 257);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Очистить";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // GenPassword
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(450, 327);
+            this.ClientSize = new System.Drawing.Size(450, 319);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.AllCheck);
             this.Controls.Add(this.SpecialSymbolsCheck);
             this.Controls.Add(this.NumbersCheck);
@@ -204,5 +226,7 @@
         private System.Windows.Forms.CheckBox NumbersCheck;
         private System.Windows.Forms.CheckBox SpecialSymbolsCheck;
         private System.Windows.Forms.CheckBox AllCheck;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
